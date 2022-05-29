ebaa589e1858f03cdcbf747c50b4bad91a323a9e
Ruchir Sanjeev Attri <ruchir.attri99@gmail.com>
2022-05-29 21:57:01 +0530

    iris dataset classification using SVM

--- SVM/sample.py
+++ SVM/sample.py
@@ -23,7 +23,12 @@ X = scaler.transform(X)
 # %%
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.3,random_state=0)
 # %%
-clasification = SVC(kernel='linear',C=1.0,random_state=0)
-clasification.fit(X_train,Y_train)
+classification = SVC(kernel='linear',C=1.0,random_state=0)
+classification.fit(X_train,Y_train)
 # %%
-accuracy = metrics.accuracy_score(Y_train,clasification.predict(X_train))
+accuracy = metrics.accuracy_score(Y_train,classification.predict(X_train))
+accuracy
+# %%
+accuracyTest = metrics.accuracy_score(Y_test,classification.predict(X_test))
+accuracyTest
+# %%
